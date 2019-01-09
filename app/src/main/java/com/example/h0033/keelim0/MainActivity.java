f13968a9d13ec0c65b06f3eaca5475de8f6007ae
Keel_im <kimh00335@gmail.com>
2019-01-09 14:52:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/h0033/keelim0/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.h0033.keelim0;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "버튼이 눌려졌습니다. ", Toast.LENGTH_LONG).show();
+    }
+}
