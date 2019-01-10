a606a819316206a1ce0a151cf79e40de0f02da64
Keel_im <kimh00335@gmail.com>
2019-01-10 14:43:24 +0900

    up

--- README.md
+++ README.md
@@ -8,7 +8,14 @@
 - Kotlin -> Basic Grammar
 
 ##2019 01 09
-1. view 설정을 할 것
-2. 코틀린?
-3. Project goals
+1. view 설정
+2. 코틀린? ->
+3. Project goals:
 
+##2019 01
+1. configurate view
+2. configurate Service -> Firebase 사용(백엔드 프로그래밍)
+3. configurate details
+
+##2019 01 10
+1. 프로젝트 화면 설정 --> main
--- app/src/main/java/com/example/h0033/keelim0/scroll.java
+++ app/src/main/java/com/example/h0033/keelim0/scroll.java
@@ -3,11 +3,11 @@ package com.example.h0033.keelim0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class scroll extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scroll);
     }
 }
