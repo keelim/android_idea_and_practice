2bd830df415a8d8e88f6865d80b2b12ebd2a37a1
Keel_im <kimh00335@gmail.com>
2019-01-11 11:55:53 +0900

    2019 01 11

--- app/src/main/java/com/example/h0033/keelim0/Frame.java
+++ app/src/main/java/com/example/h0033/keelim0/Frame.java
@@ -3,11 +3,11 @@ package com.example.h0033.keelim0;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class scroll extends AppCompatActivity {
+public class Frame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scroll);
+        setContentView(R.layout.activity_frame);
     }
 }
