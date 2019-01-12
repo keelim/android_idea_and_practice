dd178cccbab775106761a1776dbca0cfe5223cd1
Keel_im <kimh00335@gmail.com>
2019-01-12 17:00:58 +0900

    up
    up

--- app/src/main/java/com/example/h0033/myapplication/MainActivity.java
+++ app/src/main/java/com/example/h0033/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.h0033.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        RadioButton button1 = (RadioButton) findViewById(R.id.radioButton);
+        boolean checked = button1.isChecked();
+
+
     }
 }
