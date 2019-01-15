afc25b81e8de1e30351514ffe53479b58951b030
Keel_im <kimh00335@gmail.com>
2019-01-15 15:48:14 +0900

    2019 01 15 3

--- app/src/main/java/com/example/h0033/myapplication/main_activity.java
+++ app/src/main/java/com/example/h0033/myapplication/main_activity.java
@@ -1,10 +1,12 @@
 package com.example.h0033.myapplication;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class main_activity extends AppCompatActivity {
 
@@ -25,5 +27,13 @@ public class main_activity extends AppCompatActivity {
 
     }
 
-    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101){
+            String name = data.getStringExtra("name");
+            Toast.makeText(getApplicationContext(), "메뉴화면으로 부터의 응답" + name , Toast.LENGTH_LONG).show();
+        }
+    }
 }
