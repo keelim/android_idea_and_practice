2fec9a302a57aaa67da61cace5efbf46c1e3aef7
Keel_im <kimh00335@gmail.com>
2019-01-15 14:23:01 +0900

    up

---
+++ app/src/main/java/com/example/h0033/myapplication/menuActivity.java
@@ -0,0 +1,13 @@
+package com.example.h0033.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class menuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
