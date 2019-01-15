b06ef7f8e1e3bd5d9d924c0a2aeda466c81e1ba0
Keel_im <kimh00335@gmail.com>
2019-01-15 14:48:25 +0900

    2019 01 15 2

---
+++ app/src/main/java/com/example/h0033/myapplication/main_activity.java
@@ -0,0 +1,29 @@
+package com.example.h0033.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), menuActivity.class);
+                startActivityForResult(intent, 101);//화면을 구분하는 구분자 응답을 받을 때?
+
+            }
+        });
+
+    }
+
+    
+}
--- app/src/main/java/com/example/h0033/myapplication/menuActivity.java
+++ app/src/main/java/com/example/h0033/myapplication/menuActivity.java
@@ -1,7 +1,11 @@
 package com.example.h0033.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menuActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class menuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("name", "keelim"); //시스템에서 활용하지 않는다.
+
+                setResult(Activity.RESULT_OK, intent);
+
+
+                finish(); //액티비티 스택 첫번 째 화면은 깔리게 된다. menu 화면을 종료 하는 곳
+            }
+        });
+
+
     }
 }
