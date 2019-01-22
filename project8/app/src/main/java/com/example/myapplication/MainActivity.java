4e3a5a8dcdbe6db12438d2c8b24d36f230bf5d20
Keel_im <kimh00335@gmail.com>
2019-01-22 11:45:21 +0900

    2019 01 22 2

--- project8/app/src/main/java/com/example/myapplication/MainActivity.java
+++ project8/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    MainFragment fragment;
+    MenuFragment fragment2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        fragment = new MainFragment();
+        fragment2 = new MenuFragment();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+
+            }
+        });
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+
+
+            }
+        });
     }
 }
---
+++ project8/app/src/main/java/com/example/myapplication/MainFragment.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+    //인플레이션을 통하여 xml을 연결해야 한다.
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
+
+        return root;
+    }
+}
