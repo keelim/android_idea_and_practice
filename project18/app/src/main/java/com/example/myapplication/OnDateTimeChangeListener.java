fe1b41549eba17cb52d29253294ba7391ea4bffc
Keel_im <kimh00335@gmail.com>
2019-01-29 14:39:40 +0900

    2019 01 29 1

---
+++ project18/app/src/main/java/com/example/myapplication/OnDateTimeChangeListener.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+public interface OnDateTimeChangeListener {
+
+    public void onDateTimeChange(DateTimePicker view, int monthOfYear,int dayOfMonth, int hour, int minute);
+
+
+}
