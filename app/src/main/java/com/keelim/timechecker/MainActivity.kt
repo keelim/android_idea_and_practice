3bc6a3d2e0f9a88a5ae95fd4d7c3fe5da1351b1f
keelim <kimh00335@gmail.com>
2020-01-13 13:14:37 +0900

    2020 01 13

--- app/src/main/java/com/keelim/timechecker/MainActivity.kt
+++ app/src/main/java/com/keelim/timechecker/MainActivity.kt
@@ -1,34 +1,106 @@
 package com.keelim.timechecker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var timeThread: Thread
+    private var isRunning = true
+    private lateinit var handler: TimerHandler
     private lateinit var mFirebaseAnalytics: FirebaseAnalytics
-    private lateinit var fragmentTransaction: FragmentTransaction
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            val intent = Intent(this, TimerActivity::class.java)
-            startActivity(intent)
-        }
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        btn_start.setOnClickListener { v: View ->
+            v.visibility = View.GONE
+            btn_stop.visibility = View.VISIBLE
+            btn_record.visibility = View.VISIBLE
+            btn_pause.visibility = View.VISIBLE
+            handler = TimerHandler()
+            timeThread = Thread(TimerThread())
+            timeThread.start()
+        }
+
+        btn_stop.setOnClickListener { v ->
+            v.visibility = View.GONE
+            btn_record.visibility = View.GONE
+            btn_start.visibility = View.VISIBLE
+            btn_pause.visibility = View.GONE
+            recordView.text = ""
+            timeThread.interrupt()// thread control
+        }
+
+        btn_record.setOnClickListener {
+            recordView.text =
+                "${recordView.text}${timeView.text}\n"
+        }
+
+        btn_pause.setOnClickListener {
+            isRunning = !isRunning
+            if (isRunning) {
+                btn_pause!!.text = "일시정지"
+            } else {
+                btn_pause!!.text = "시작"
+            }
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+    private inner class TimerThread : Runnable {
+        override fun run() {
+            var i = 0
+            while (true) {
+                while (isRunning) { //일시정지를 누르면 멈춤
+                    val msg = Message()
+                    msg.arg1 = i++
+                    handler.sendMessage(msg)
+                    try {
+                        Thread.sleep(10)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                        runOnUiThread {
+                            timeView!!.text = ""
+                            timeView!!.text = "00:00:00:00"
+                        }
+                        return  // 인터럽트 받을 경우 return
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressLint("HandlerLeak")
+    inner class TimerHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            val mSec = msg.arg1 % 100
+            val sec = msg.arg1 / 100 % 60
+            val min = msg.arg1 / 100 / 60
+            val hour = msg.arg1 / 100 / 360
+            //1000이 1초 1000*60 은 1분 1000*60*10은 10분 1000*60*60은 한시간
+
+            val result = String.format("%02d:%02d:%02d:%02d", hour, min, sec, mSec)
+            if (result == "00:01:15:00") {
+                Toast.makeText(applicationContext, "1분 15초가 지났습니다.", Toast.LENGTH_SHORT).show()
+            }
+            timeView!!.text = result
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -43,8 +115,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun replaceFragment(fragment: Fragment) {
-        fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.main_container, fragment)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
+
+
 }
+
+
