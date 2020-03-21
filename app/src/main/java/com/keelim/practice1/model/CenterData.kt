3d4b90f8295489f60e20d5b1c565585933ff0f08
h0033 <kimh00335@gmail.com>
2020-03-21 18:34:11 +0900

    2020 03 21

--- app/src/main/java/com/keelim/practice1/model/CenterData.kt
+++
@@ -1,6 +0,0 @@
-package com.keelim.practice1.model
-
-/**
- * Created by kimok_000 on 2015-11-28.
- */
-class CenterData(val title: String, val content: String, val map: String, val tel: String)
--- app/src/main/java/com/keelim/practice1/model/FindInfoData.kt
+++
@@ -1,6 +0,0 @@
-package com.keelim.practice1.model
-
-/**
- * Created by kimok_000 on 2015-11-28.
- */
-class FindInfoData(val title: String, val content: String, val button0: String, val button1: String)
